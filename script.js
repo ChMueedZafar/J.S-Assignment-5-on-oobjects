02ea9e5ee0c6ca9b8505b49656ea00ed9b5021dd
ChMueedZafar <chmueedzafar@gmail.com>
2024-05-14 23:57:03 +0500

    second commit

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // Question no 1:-
+let myobj = {};
 let student = {
 name: 'mueed',
 age: 18 ,
 profession: "programing"
 }
+// Question no 2:-
+console.log(Object.values(student));
+student.name = "zafar"
+student.age = 20
+student.profession = "software engireing"
+student.location = "Jaranwala"
 console.log(student);
+// Question no 3:-
+
