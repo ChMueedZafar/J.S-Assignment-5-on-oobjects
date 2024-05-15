9d49b91cd700b6b821ca9b99e986d09674a4a212
ChMueedZafar <chmueedzafar@gmail.com>
2024-05-15 13:30:49 +0500

    second commit

--- script.js
+++ script.js
@@ -13,4 +13,29 @@ student.profession = "software engireing"
 student.location = "Jaranwala"
 console.log(student);
 // Question no 3:-
+const myobje = {
+    message : "hello world"
+};
+console.log(myobje.message);
+let myobject ={
+    with: 5,
+    height: 10,
+    calclueArea: function(){
+        return this.with  * this.height;
+    }  
+};
+console.log(myobject.calclueArea());
+
+//  Question no 4:-
+const person ={
+    name : "mueed",
+    age: 19,
+    gerend: "Male",
+    city : "jaranwala"
+}
+for (let key in person){
+if (person.hasOwnProperty(key)){
+    console.log(person[key]);
+}
+}
 
